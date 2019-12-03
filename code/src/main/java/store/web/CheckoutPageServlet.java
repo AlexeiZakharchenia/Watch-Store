e30d00aeb5a5679d073a688c82f2b04c35adb24b
Alexei Zakharchenia <alexeizakh@mail.ru>
2019-12-03 09:32:17 +0300

    Created logic on checkout page. Created overview of order page logic

--- code/src/main/java/store/web/CheckoutPageServlet.java
+++ code/src/main/java/store/web/CheckoutPageServlet.java
@@ -3,8 +3,7 @@ package store.web;
 import store.cart.Cart;
 import store.cart.CartService;
 import store.cart.HttpSessionCartService;
-import store.order.OrderService;
-import store.order.OrderServiceImpl;
+import store.order.*;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -36,5 +35,55 @@ public class CheckoutPageServlet extends HttpServlet {
         request.getRequestDispatcher("/WEB-INF/pages/checkout.jsp").forward(request, response);
     }
 
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Cart cart = cartService.getCart(request);
+        String deliveryModeString = request.getParameter("deliveryMode");
+        boolean hasError = false;
+        DeliveryMode deliveryMode = null;
+        if (deliveryModeString != null) {
+            int index = deliveryModeString.indexOf('(');
+            deliveryModeString = deliveryModeString.substring(0, index);
+            deliveryMode = DeliveryMode.valueOf(deliveryModeString);
+        } else {
+            hasError = true;
+            request.setAttribute("deliveryModeError", "Unknown delivery mode");
+        }
+
+        String name = request.getParameter("name");
+        if (name == null || name.isEmpty()) {
+            hasError = true;
+            request.setAttribute("nameError", "Name is required");
+        }
+        String address = request.getParameter("address");
+        if (address == null || address.isEmpty()) {
+            hasError = true;
+            request.setAttribute("addressError", "Address is required");
+        }
+        PaymentMethod paymentMethod = null;
+        String paymentMethodString = request.getParameter("paymentMethod");
+        if (paymentMethodString != null) {
+            paymentMethod = PaymentMethod.valueOf(paymentMethodString);
+        } else {
+            hasError = true;
+            request.setAttribute("paymentMethodError", "Unknown payment method");
+        }
+        if (hasError) {
+            doGet(request, response);
+            return;
+        }
+
+
+        Order order = orderService.createOrder(cart, deliveryMode);
+        order.setName(name);
+        order.setAddress(address);
+        order.setDeliveryMode(deliveryMode);
+        order.setDeliveryCost((deliveryMode.getDeliveryCost()));
+        order.setOrderTotal(order.getDeliveryCost().add(order.getTotalPrice()));
+        order.setPaymentMethod(paymentMethod);
+        orderService.placeOrder(order);
+        cartService.clearCart(cart, request);
+        response.sendRedirect(request.getRequestURI() + "/orderOverview/" + order.getSecureId());
+    }
 
 }
